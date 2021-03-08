0f6b8ec8dc0685f6dbc208b7fbe8c8322e2c3477
bortalberto <bortalberto@gmail.com>
2021-03-08 10:54:01 +0100

    Master save

--- alignment.py
+++ alignment.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import keras
 def isnotebook():
     try:
         shell = get_ipython().__class__.__name__
@@ -112,6 +113,49 @@ def align_runs(run_list, iterations=1):
         pickle.dump(correction, open(os.path.join(data_folder, "alignment", f"{run}"), 'wb'))
     return corr_tracks, track_pd
 
+# def align_runs_neural(run_list, iterations=1):
+#     """
+#     Aligns the run in the list, each one will have the same correction array
+#     :param run_list:
+#     :return:
+#     """
+#     c_pd_list = []
+#     t_pd_list = []
+#     for run_number in run_list:
+#         # loads all the data in the macrorun
+#         c_pd_list.append(pd.read_pickle("{}/raw_root/{}/cluster_pd_1D.pickle.gzip".format(data_folder, run_number), compression="gzip"))
+#         t_pd_list.append(pd.read_pickle("{}/raw_root/{}/tracks_pd_1D.pickle.gzip".format(data_folder, run_number), compression="gzip"))
+#     cluster_pd = pd.concat(c_pd_list)
+#     track_pd = pd.concat(t_pd_list)
+#
+#     # Add infos about the cluster position (to be fixed when adding angle)
+#     cluster_pd["cl_pos_x_cm"] = cluster_pd.cl_pos_x * 0.0650
+#     cluster_pd["cl_pos_y_cm"] = cluster_pd.cl_pos_y * 0.0650
+#     cluster_pd["cl_pos_z_cm"] = cluster_pd.planar * 10
+#
+#     # Initialize the dict for correction and displacement
+#     displ=build_displacement_pd(track_pd)
+#     correction = {
+#         0: {"x": 0, "y": 0},
+#         1: {"x": 0, "y": 0},
+#         2: {"x": 0, "y": 0},
+#         3: {"x": 0, "y": 0}
+#
+#     }
+#
+#     # Performs N rounds of alignment
+#     for j in tqdm(range(0, iterations), desc="Iterations"):
+#         for planar in tqdm((0, 1), desc= "Planar"):
+#             for view in ("x", "y"):
+#                 cluster_pd.loc[cluster_pd.planar == planar, f"cl_pos_{view}_cm"] = cluster_pd.loc[cluster_pd.planar == planar, f"cl_pos_{view}_cm"] - displ[f"displ_planar_{planar}_{view}"]
+#                 correction[planar][view] += displ[f"displ_planar_{planar}_{view}"]
+#             corr_tracks = build_tracks_pd(cluster_pd)
+#             displ = build_displacement_pd(corr_tracks)
+#     for run in run_list:
+#         pickle.dump(correction, open(os.path.join(data_folder, "alignment", f"{run}"), 'wb'))
+#     return corr_tracks, track_pd
+
+
 def apply_correction(cluster_pd, corr):
     cluster_pd["cl_pos_x_cm"] = cluster_pd.cl_pos_x * 0.0650
     cluster_pd["cl_pos_y_cm"] = cluster_pd.cl_pos_y * 0.0650
@@ -139,6 +183,8 @@ def display_results(corr_tracks, track_pd):
             fig_list.append(fig)
     for fig in fig_list:
         fig.show()
+
+
 if __name__ == "__main__":
     corr,old = align_runs((29,),1)
     display_results(corr, old)
