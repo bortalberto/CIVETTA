fd33bd3d18528cbdf29ab4197008785d81f308e3
borta <bortalberto@gmail.com>
2021-11-15 21:08:12 +0100

    Debug print tolti, nuovi parametro filtraggio tracce

--- alignment.py
+++ alignment.py
@@ -279,9 +279,9 @@ def calibrate_alignment_run(run, rounds, cpu, data_folder, downsampling):
                  correction[f"{planar}_{view}"]=np.poly1d([0,0])
         for pl in tqdm([3,2,1], leave = False , desc="Planars"):
             tracks_pd = alignment_istance.fit_tracks_manager(cl_pd_2D,pl)
-            print(tracks_pd.shape)
-            tracks_pd = alignment_istance.filter_tracks(tracks_pd, cut=-100)
-            print(tracks_pd.shape)
+            # print(tracks_pd.shape)
+            tracks_pd = alignment_istance.filter_tracks(tracks_pd, cut=-0.2)
+            # print(tracks_pd.shape)
             correction.update(alignment_istance.calc_correction(tracks_pd, planar=pl))
             cl_pd_2D = alignment_istance.apply_correction(cl_pd_2D, pl, correction)
         alignment_istance.corrections.append(correction)
