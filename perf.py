67ecf25fae885003aa2bb5db5aac9bcd415f0927
borta <bortalberto@gmail.com>
2021-12-28 18:02:58 +0100

    Debug print

--- perf.py
+++ perf.py
@@ -584,6 +584,7 @@ def calculte_eff(run, data_folder, put, cpu_to_use, nsigma_put=5, nsigma_tracker
         # Estraggo mean e sigma sulla planare sotto test, serve per stabilire l'efficienza
         view = "x"
         popt_list, pcov_list, res_list, R_list,chi_list, deg_list = double_gaus_fit_root(tracks_pd_res, view)
+        print (len(popt_list), len(pcov_list), len(res_list), len(R_list),len(chi_list), len(deg_list))
         put_mean_x = ((popt_list[put][1] * popt_list[put][0] * popt_list[put][2]) + (popt_list[put][4] * popt_list[put][3] * popt_list[put][5])) / (popt_list[put][0] * popt_list[put][2] + popt_list[put][3] * popt_list[put][5])
         put_sigma_x = ((popt_list[put][2] * popt_list[put][0] * popt_list[put][2]) + (popt_list[put][5] * popt_list[put][3] * popt_list[put][5])) / (popt_list[put][0] * popt_list[put][2] + popt_list[put][3] * popt_list[put][5])
         popt_list_put_x=popt_list
