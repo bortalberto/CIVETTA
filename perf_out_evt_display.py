6409b840636a39d8360535dca4de428e33062be3
bortalberto <bortalberto@gmail.com>
2022-01-25 11:18:22 +0100

    Updated to display every event

--- perf_out_evt_display.py
+++ perf_out_evt_display.py
@@ -34,32 +34,59 @@ class event_visualizer:
     """
     Class to manage the event visualizer without pre-elaborate the data each time
     """
-    def __init__(self, cluster_pd_1D,cluster_pd_match, tracks_pd, hit_pd, put, correction):
-        self.cluster_pd_1D  =cluster_pd_1D
+
+
+    def __init__(self, cluster_pd_1D,cluster_pd_match, tracks_pd, hit_pd, eff_pd, put, correction):
+        self.cluster_pd_1D  = cluster_pd_1D
         self.cluster_pd_1D["cl_pos_x_cm"] = cluster_pd_1D.cl_pos_x * 0.0650
         self.cluster_pd_1D["cl_pos_y_cm"] = cluster_pd_1D.cl_pos_y * 0.0650
         self.cluster_pd_match = cluster_pd_match
         self.tracks_pd = tracks_pd
         self.put = put
+        self.eff_pd = eff_pd
         self.tracks_pd["prev_pos_put_x"] = self.tracks_pd["fit_x"].apply(lambda x: x[0]) * self.put * 10 + tracks_pd["fit_x"].apply(
             lambda x: x[1])
         self.tracks_pd["prev_pos_put_y"] = self.tracks_pd["fit_y"].apply(lambda x: x[0]) * self.put * 10 + tracks_pd["fit_y"].apply(
             lambda x: x[1])
         self.correction = correction
         self.data_pd = hit_pd
+        self.event_eff_list = self.eff_pd["count"].unique()
 
 
 
     def plot_evt(self, event):
+        """
+        Plot the event, including the supposed position and the non efficient clusters
+        """
         subfig = make_subplots(specs=[[{"secondary_y": True}]])
         #     event=int(np.random.choice(cluster_pd_1D_match[(cluster_pd_1D_match.cl_pos_x_cm.notna()) &(cluster_pd_1D_match.cl_size>5)]["count"].values))
         #     event= 511
-        this_evt_tracks_pd = self.tracks_pd[self.tracks_pd["count"] == event]
-        this_evt_cluster = self.cluster_pd_match[
-            (self.cluster_pd_match["count"] == event) & (self.cluster_pd_match.cl_pos_x_cm.notna())]
-        hit_ids = this_evt_cluster.hit_ids.values[0]
+        if np.isin(event, self.event_eff_list):
+            evt_for_eff = True
+            efficient_x = self.eff_pd.loc[self.eff_pd["count"] == event].eff_x.values[0]
+        else:
+            evt_for_eff = False
+            efficient_x = False
+
+        if evt_for_eff and efficient_x:
+            this_evt_tracks_pd = self.tracks_pd[self.tracks_pd["count"] == event]
+            this_evt_cluster = self.cluster_pd_match[
+                (self.cluster_pd_match["count"] == event) & (self.cluster_pd_match.cl_pos_x_cm.notna())]
+
+        if evt_for_eff and not efficient_x:
+            this_evt_tracks_pd = self.tracks_pd[self.tracks_pd["count"] == event]
+            cluster_pd_1D_c = self.cluster_pd_1D[(self.cluster_pd_1D["count"] == event) & (self.cluster_pd_1D.planar == self.put)]
+            clusters = cluster_pd_1D_c.apply(lambda x: perf.apply_correction_eff(x, this_evt_tracks_pd.prev_pos_put_x.values[0], this_evt_tracks_pd.prev_pos_put_y.values[0], self.correction), axis=1)
+            clusters["prev_pos_x_cm"] = this_evt_tracks_pd.prev_pos_put_x.values[0]
+            clusters["prev_pos_y_cm"] = this_evt_tracks_pd.prev_pos_put_y.values[0]
+            this_evt_cluster = clusters.iloc[(clusters['cl_pos_x_cm'] - this_evt_tracks_pd.prev_pos_put_x.values[0]).abs().argsort()[:1]]
+
         data_pd_evt = self.data_pd[(self.data_pd["count"] == event) & (self.data_pd["planar"] == self.put) & (self.data_pd["strip_x"] > 0) & (
                     self.data_pd["l1ts_min_tcoarse"] > 1370) & (self.data_pd["l1ts_min_tcoarse"] < 1440)]
+        if evt_for_eff:
+            hit_ids = this_evt_cluster.hit_ids.values[0]
+        else:
+            hit_ids = [-1]
         data_pd_evt["cluster_eff"] = data_pd_evt["hit_id"].isin(hit_ids)
         color_discrete_map = {False: 'cyan', True: 'red'}
         data_pd_evt["time"] = (1569 - data_pd_evt["l1ts_min_tcoarse"])
@@ -91,13 +118,14 @@ class event_visualizer:
             range=[1569 - 1440, 1569 - 1370],
             side="right"
         ), secondary_y=False)
-        pos_x = de_correct_process(this_evt_cluster.cl_pos_x_cm.values[0], this_evt_tracks_pd.prev_pos_put_y.values[0],
-                                   self.correction, self.put)
-        pos_x_prev = de_correct_process(this_evt_tracks_pd.prev_pos_put_x.values[0],
-                                        this_evt_tracks_pd.prev_pos_put_y.values[0], self.correction, self.put)
+        if evt_for_eff:
+            pos_x = de_correct_process(this_evt_cluster.cl_pos_x_cm.values[0], this_evt_tracks_pd.prev_pos_put_y.values[0],
+                                       self.correction, self.put)
+            pos_x_prev = de_correct_process(this_evt_tracks_pd.prev_pos_put_x.values[0],
+                                            this_evt_tracks_pd.prev_pos_put_y.values[0], self.correction, self.put)
 
-        subfig.add_vline(pos_x_prev[0] / 0.0650, line=dict(color="orange", dash="dot", width=0.7), name="Prev pos")
-        subfig.add_vline(pos_x[0] / 0.0650, line=dict(color="red", dash="dot", width=0.7), name="Nearest cl pos")
+            subfig.add_vline(pos_x_prev[0] / 0.0650, line=dict(color="orange", dash="dot", width=0.7), name="Prev pos")
+            subfig.add_vline(pos_x[0] / 0.0650, line=dict(color="red", dash="dot", width=0.7), name="Nearest cl pos")
 
         subfig.update_layout(title=f"Event {event}, planar {self.put}, vista X")
         subfig.update_layout(template="plotly_dark")
@@ -106,27 +134,44 @@ class event_visualizer:
 
         # Y
         subfig = make_subplots(specs=[[{"secondary_y": True}]])
-        #     event=632922
-        this_evt_tracks_pd = self.tracks_pd[self.tracks_pd["count"] == event]
-        this_evt_cluster = self.cluster_pd_match[
-            (self.cluster_pd_match["count"] == event) & (self.cluster_pd_match.cl_pos_y_cm.notna())]
-        hit_ids = this_evt_cluster.hit_ids.values[0]
-        data_pd_evt = self.data_pd[(self.data_pd["count"] == event) & (self.data_pd["planar"] == self.put) & (self.data_pd["strip_y"] > 0) & (
-                    self.data_pd["l1ts_min_tcoarse"] > 1370) & (self.data_pd["l1ts_min_tcoarse"] < 1440)]
-        data_pd_evt["nearest_cluster"] = data_pd_evt["hit_id"].isin(hit_ids)
-        data_pd_evt["nearest_cluster_str"] = data_pd_evt["nearest_cluster"].apply(
-            lambda x: "Nearest cluster" if x else "Other hits")
+        if np.isin(event, self.event_eff_list):
+            evt_for_eff = True
+            efficient_y = self.eff_pd.loc[self.eff_pd["count"] == event].eff_y.values[0]
+        else:
+            evt_for_eff = False
+            efficient_y = False
 
-        color_discrete_map = {"Other hits": 'cyan', "Nearest cluster": 'red'}
+        if evt_for_eff and efficient_y:
+            this_evt_tracks_pd = self.tracks_pd[self.tracks_pd["count"] == event]
+            this_evt_cluster = self.cluster_pd_match[
+                (self.cluster_pd_match["count"] == event) & (self.cluster_pd_match.cl_pos_y_cm.notna())]
+
+        if evt_for_eff and not efficient_y:
+            this_evt_tracks_pd = self.tracks_pd[self.tracks_pd["count"] == event]
+            cluster_pd_1D_c = self.cluster_pd_1D[(self.cluster_pd_1D["count"] == event) & (self.cluster_pd_1D.planar == self.put)]
+            clusters = cluster_pd_1D_c.apply(lambda x: perf.apply_correction_eff(x, this_evt_tracks_pd.prev_pos_put_x.values[0], this_evt_tracks_pd.prev_pos_put_y.values[0], self.correction), axis=1)
+            clusters["prev_pos_x_cm"] = this_evt_tracks_pd.prev_pos_put_x.values[0]
+            clusters["prev_pos_y_cm"] = this_evt_tracks_pd.prev_pos_put_y.values[0]
+            this_evt_cluster = clusters.iloc[(clusters['cl_pos_y_cm'] - this_evt_tracks_pd.prev_pos_put_y.values[0]).abs().argsort()[:1]]
+
+        data_pd_evt = self.data_pd[(self.data_pd["count"] == event) & (self.data_pd["planar"] == self.put) & (self.data_pd["strip_y"] > 0) & (
+                self.data_pd["l1ts_min_tcoarse"] > 1370) & (self.data_pd["l1ts_min_tcoarse"] < 1440)]
+        if evt_for_eff:
+            hit_ids = this_evt_cluster.hit_ids.values[0]
+        else:
+            hit_ids = [-1]
+        data_pd_evt["cluster_eff"] = data_pd_evt["hit_id"].isin(hit_ids)
+        color_discrete_map = {False: 'cyan', True: 'red'}
         data_pd_evt["time"] = (1569 - data_pd_evt["l1ts_min_tcoarse"])
-        fig = px.scatter(data_pd_evt, "strip_y", "charge_SH", color="nearest_cluster_str",
+
+        fig = px.scatter(data_pd_evt, "strip_y", "charge_SH", color="cluster_eff",
                          color_discrete_map=color_discrete_map)
         fig2 = px.scatter(data_pd_evt, "strip_y", "time", color_discrete_sequence=["grey"])
         fig.update_traces(yaxis="y2")
         #     fig2.update_layout(name="time")
-        #     fig.data[-1].name = 'Nearest cluster'
-        #     if len(fig.data)>1:
-        #         fig.data[-2].name = 'Other hits'
+        fig.data[-1].name = 'Nearest cluster'
+        if len(fig.data) > 1:
+            fig.data[-2].name = 'Other hits'
 
         fig2.data[-1].name = 'Time'
         fig2.data[-1].showlegend = True
@@ -146,13 +191,14 @@ class event_visualizer:
             range=[1569 - 1440, 1569 - 1370],
             side="right"
         ), secondary_y=False)
-        pos_x = de_correct_process(this_evt_tracks_pd.prev_pos_put_x.values[0], this_evt_cluster.cl_pos_y_cm.values[0],
-                                   self.correction, self.put)
-        pos_x_prev = de_correct_process(this_evt_tracks_pd.prev_pos_put_x.values[0],
-                                        this_evt_tracks_pd.prev_pos_put_y.values[0], self.correction, self.put)
+        if evt_for_eff:
+            pos_x = de_correct_process(this_evt_tracks_pd.prev_pos_put_x.values[0], this_evt_cluster.cl_pos_y_cm.values[0],
+                                       self.correction, self.put)
+            pos_x_prev = de_correct_process(this_evt_tracks_pd.prev_pos_put_x.values[0],
+                                            this_evt_tracks_pd.prev_pos_put_y.values[0], self.correction, self.put)
 
-        subfig.add_vline(pos_x_prev[1] / 0.0650, line=dict(color="orange", dash="dot", width=0.7), name="Prev pos")
-        subfig.add_vline(pos_x[1] / 0.0650, line=dict(color="red", dash="dot", width=0.7), name="Nearest cl pos")
+            subfig.add_vline(pos_x_prev[1] / 0.0650, line=dict(color="orange", dash="dot", width=0.7), name="Prev pos")
+            subfig.add_vline(pos_x[1] / 0.0650, line=dict(color="red", dash="dot", width=0.7), name="Nearest cl pos")
 
         subfig.update_layout(title=f"Event {event}, planar {self.put}, vista Y")
         subfig.update_layout(template="plotly_dark")
