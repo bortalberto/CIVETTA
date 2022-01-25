c317cfcf4ce02c3e86eb4feca35912120934d420
bortalberto <bortalberto@gmail.com>
2022-01-25 12:12:13 +0100

    Efficiency calculation

--- perf_out_evt_display.py
+++ perf_out_evt_display.py
@@ -247,6 +247,7 @@ class eff_calculation:
         with open(self.log_path, "r") as logfile:
             view = "x"
             tol_x = [float(line.split("+/-")[1].split()[0]) for line in logfile if f"Residual {view}" in line]
+        with open(self.log_path, "r") as logfile:
             view = "y"
             tol_y = [float(line.split("+/-")[1].split()[0]) for line in logfile if f"Residual {view}" in line]
         time_win = (1440 - 1370) * 6.25 * 1e-9
