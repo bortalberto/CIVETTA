50ff433bf3b0ea6962b02960909ea45a03410d04
bortalberto <bortalberto@gmail.com>
2022-01-26 15:23:46 +0100

    Multiple put with one class istance

--- perf_out_evt_display.py
+++ perf_out_evt_display.py
@@ -467,7 +467,7 @@ class res_measure:
         # plt.ion()
         # plt.show()
         ax.set_title(f"Fit view {view}, planar{pl}")
-        ax.text(y=np.max(y) * 0.7, x=0 +0.1,
+        ax.text(y=np.max(y) * 0.7, x=0 + popt[2],
                  s=f"R^2={R_list:.4f}\nNorm_0={popt[0]:.2f}, Mean_0={popt[1] * 10000:.2f}um, Sigma_0={(popt[2]) * 10000:.2f}um"
                    f"\nNorm_1={popt[3]:.2f}, Mean_1={popt[4] * 10000:.2f}um, Sigma_1={abs(popt[5]) * 10000:.2f}um"
                    f"\nChi_sqrt={chi_list:.3e}, Chi_sqrt/NDoF = {chi_list / deg_list:.3e}",
