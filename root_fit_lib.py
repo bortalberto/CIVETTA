906d82b573b3d85d6493ea9407e0e485154f0474
bortalberto <bortalberto@gmail.com>
2022-01-31 16:57:17 +0100

    Added sigle fit range estimation

--- root_fit_lib.py
+++ root_fit_lib.py
@@ -79,7 +79,7 @@ def double_gaus_fit_root(tracks_pd, view="x", put=-1, sigma_def=0.2, pl_list=ran
 #             popt, pcov = curve_fit(doublegaus, x, y,sigma=error,p0=[a_0, mean_0, sigma_0, a_1, mean_1, sigma_1, c], bounds=(lower_bound, upper_bound))
 
             lower_bound=[np.max(y)/4*3,x[np.argmax(y)]-sigma_0/10,0,               0,x[np.argmax(y)]-sigma_0/10,         0,     0]
-            upper_bound=[np.max(y)    ,x[np.argmax(y)]+sigma_0/10,sigma_0/2,       np.max(y)/4,x[np.argmax(y)]+sigma_0/10,sigma_0*4,     200]
+            upper_bound=[np.max(y)    ,x[np.argmax(y)]+sigma_0/10,sigma_0,       np.max(y)/4,x[np.argmax(y)]+sigma_0/10,sigma_0*4,     200]
             # print(lower_bound)
             # print (upper_bound)
             popt, chi_sqr = root_fit(data,[a_0, mean_0, sigma_0, a_1, mean_1, sigma_1, c], lower_bound, upper_bound, sigma_def )
