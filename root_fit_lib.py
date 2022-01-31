1333a3b679208dd9cf80efa7dce6440b60df218f
bortalberto <bortalberto@gmail.com>
2022-01-31 17:01:35 +0100

    Added sigle fit range estimation

--- root_fit_lib.py
+++ root_fit_lib.py
@@ -78,8 +78,8 @@ def double_gaus_fit_root(tracks_pd, view="x", put=-1, sigma_def=0.2, pl_list=ran
 #             upper_bound=[np.inf,  x[np.argmax(y)]+0.01, 1, np.inf,x[np.argmax(y)]+0.01,2,100]
 #             popt, pcov = curve_fit(doublegaus, x, y,sigma=error,p0=[a_0, mean_0, sigma_0, a_1, mean_1, sigma_1, c], bounds=(lower_bound, upper_bound))
 
-            lower_bound=[np.max(y)/4*3,x[np.argmax(y)]-sigma_0/10,0,               0,x[np.argmax(y)]-sigma_0/10,         0,     0]
-            upper_bound=[np.max(y)    ,x[np.argmax(y)]+sigma_0/10,sigma_0,       np.max(y)/4,x[np.argmax(y)]+sigma_0/10,sigma_0*4,     200]
+            lower_bound=[np.max(y)/5*4,x[np.argmax(y)]-sigma_0/10,0,               0,x[np.argmax(y)]-sigma_0/10,         0,     0]
+            upper_bound=[np.max(y)    ,x[np.argmax(y)]+sigma_0/10,sigma_0,       np.max(y)/5,x[np.argmax(y)]+sigma_0/10,sigma_def,     200]
             print(lower_bound)
             print (upper_bound)
             print ([a_0, mean_0, sigma_0, a_1, mean_1, sigma_1, c])
