d5d99d656e28fb98cb3532015c1e9e7f4084cb03
bortalberto <bortalberto@gmail.com>
2022-01-31 15:09:06 +0100

    Added sigle fit range estimation

--- perf.py
+++ perf.py
@@ -455,7 +455,7 @@ def calculte_eff(run, data_folder, put, cpu_to_use, nsigma_put=5, nsigma_tracker
         tracks_pd_res = fit_tracks_manager(cl_pd_2D_res, put)
         # Estraggo mean e sigma sulla planare sotto test, serve per stabilire l'efficienza
         view = "x"
-        sigma_set = estimate_sigma_def(tracks_pd_res)
+        sigma_set = 0.2
         popt_list, pcov_list, res_list, R_list,chi_list, deg_list = r_fit.double_gaus_fit_root(tracks_pd_res, view, sigma_def=sigma_set)
         print (len(popt_list), len(pcov_list), len(res_list), len(R_list),len(chi_list), len(deg_list))
         put_mean_x = ((popt_list[put][1] * popt_list[put][0] * popt_list[put][2]) + (popt_list[put][4] * popt_list[put][3] * popt_list[put][5])) / (popt_list[put][0] * popt_list[put][2] + popt_list[put][3] * popt_list[put][5])
