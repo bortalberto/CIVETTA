92482ab068a04892dc80c2c12147d21b7cf43f85
bortalberto <bortalberto@gmail.com>
2022-05-30 17:19:22 +0200

    Fixing time correction

--- tpc.py
+++ tpc.py
@@ -98,7 +98,9 @@ class tpc_prep:
         for thr in (0.5, 1, 2, 3):
             dict_calibrations[thr] = np.append(np.array([self.signal_width]), self.get_calibration_time_walk_courve(self.signal_width, thr))
         hit_pd["hit_time_corr"] = hit_pd.apply(lambda x: calc_corr(x, dict_calibrations, thr_tmw), axis=1)
-        hit_pd["hit_time"] = -(hit_pd["l1ts_min_tcoarse"] - 1567) * 6.25 + hit_pd["hit_time_corr"] - 800
+        hit_pd["hit_time"] = -(hit_pd["l1ts_min_tcoarse"] - 1567) * 6.25 - 800
+        # hit_pd["hit_time"] = -(hit_pd["l1ts_min_tcoarse"] - 1567) * 6.25 - hit_pd["hit_time_corr"] - 800
+
         hit_pd["hit_time_error"] = (15**2 + (hit_pd["hit_time_corr"]/3)**2 + 6.25/(12**1/2)**2 )**(1/2)
         return hit_pd
 
