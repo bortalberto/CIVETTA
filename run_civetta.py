2930e51e994ac2ede6cf391e760dea66ae2bd15f
bortalberto <bortalberto@gmail.com>
2022-07-04 12:05:42 +0200

    Adding bash runner script

--- run_civetta.py
+++ run_civetta.py
@@ -6,9 +6,9 @@ if len(sys.argv)>2:
     down_sampling = sys.argv[2]
 else:
     down_sampling = 2
-
-os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down 20")
-os.system(f"./civetta.py -ca_al {run} -cpu 35")
-os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down {down_sampling}")
-os.system(f"./civetta_perf.py {run} -perf -1 -sT 20 -sD 6 -chi -cpu 30")
+#
+# os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down 20")
+# os.system(f"./civetta.py -ca_al {run} -cpu 35")
+# os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down {down_sampling}")
+os.system(f"./civetta_perf.py {run} -perf -sT 20 -sD 6 -chi -cpu 20;")
 #./civetta_perf.py {run} -Ser;
