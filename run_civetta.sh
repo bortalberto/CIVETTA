65704b8bd84d2b70561c82ee82710179dfbf6411
bortalberto <bortalberto@gmail.com>
2022-07-04 11:21:59 +0200

    Adding bash runner script

--- run_civetta.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Run: $1";
-echo "Downsampling: $2";
-#echo "Full Name: $3";
-./civetta.py -d -a -c -c2 -cpu 30 $1 -down 20;
-./civetta.py -ca_al $1 -cpu 35;
-./civetta.py -d -a -c -c2 -cpu 30 $1 -down 2;
-./civetta_perf.py $1 -perf -1 -sT 20 -sD 6 -chi -cpu 30;
-#./civetta_perf.py $1 -Ser;
