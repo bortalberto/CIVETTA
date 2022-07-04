ebfa05387af4a0583254a7927ebc571c77988655
bortalberto <bortalberto@gmail.com>
2022-07-04 11:23:27 +0200

    Adding bash runner script

--- run_civetta.py
+++ run_civetta.py
@@ -1,13 +1,13 @@
 import os
 import sys
 
-run = sys.argv[1]
+run = sys.argv[0]
 if len(sys.argv)>2:
     down_sampling = sys.argv[2]
 else:
     down_sampling = 2
 
-os.system(f"/civetta.py -d -a -c -c2 -cpu 30 {run} -down 20")
+os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down 20")
 # ./civetta.py -ca_al $1 -cpu 35;
 # ./civetta.py -d -a -c -c2 -cpu 30 $1 -down 2;
 # ./civetta_perf.py $1 -perf -1 -sT 20 -sD 6 -chi -cpu 30;
