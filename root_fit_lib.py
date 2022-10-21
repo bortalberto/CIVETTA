8331b0cfe2ce954b93d70d737a82863c178e6583
bortalberto <bortalberto@gmail.com>
2022-10-21 17:50:36 +0200

    Debugging fit

--- root_fit_lib.py
+++ root_fit_lib.py
@@ -77,7 +77,7 @@ def double_gaus_fit_root(tracks_pd, view="x", put=-1, sigma_def=0.2, pl_list=ran
             #             y=y[4000:6000]
             mean_1 =  x[np.argmax(y)]
             mean_0 =  x[np.argmax(y)]
-            a_0 = np.max(y)
+            a_0 = np.max(y) -1
             a_1 = np.max(y) / 10
             sigma_0 = sigma_def/10
             sigma_1 = sigma_def
@@ -87,7 +87,7 @@ def double_gaus_fit_root(tracks_pd, view="x", put=-1, sigma_def=0.2, pl_list=ran
 #             popt, pcov = curve_fit(doublegaus, x, y,sigma=error,p0=[a_0, mean_0, sigma_0, a_1, mean_1, sigma_1, c], bounds=(lower_bound, upper_bound))
 
             lower_bound=[np.max(y)/5*4,x[np.argmax(y)]-sigma_0/3,0,               0,x[np.argmax(y)]-sigma_0/3,         0,     0]
-            upper_bound=[np.max(y)    ,x[np.argmax(y)]+sigma_0/3,sigma_0,       np.max(y)/5,x[np.argmax(y)]+sigma_0/3,sigma_def,     200]
+            upper_bound=[np.max(y)    ,x[np.argmax(y)]+sigma_0/3,sigma_0,       np.max(y)/5,x[np.argmax(y)]+sigma_0/3,sigma_def + sigma_def/10,     200]
             # print(pl)
             # print(lower_bound)
             # print ([a_0, mean_0, sigma_0, a_1, mean_1, sigma_1, c])
