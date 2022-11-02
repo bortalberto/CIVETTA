1b4cce3861e4677da143b4577d954f68546eb1e5
bortalberto <bortalberto@gmail.com>
2022-11-02 18:43:45 +0100

    Debugging fit

--- root_fit_lib.py
+++ root_fit_lib.py
@@ -279,12 +279,12 @@ def plot_residuals(tracks_pd_res, view,popt_list,R_list, path_out_eff, put,put_m
 
 
 def estimate_sigma_def(data):
-    std = np.std(data)
+    data = data[abs(data) < 10]
     # print (f"std {std}")
     # print (f"data:  ({len(data)})")
-    z_scores = zscore(data)
-    std = np.std(data[np.abs(z_scores) < 1])
-    data=data[np.abs(z_scores) < 1]
+    # z_scores = zscore(data)
+    # std = np.std(data[np.abs(z_scores) < 2])
+    # data=data[np.abs(z_scores) < 2]
     # print (f"std {std}")
     # print (f"data:  ({len(data)})")
     # z_scores = zscore(data)
