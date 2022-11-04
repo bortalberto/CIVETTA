2b46046941eb90d01e99e450528fe21ae0318587
borta <bortalberto@gmail.com>
2022-11-04 17:03:25 +0100

    Feather format addeding to monitor

--- tpc.py
+++ tpc.py
@@ -958,7 +958,7 @@ class plotter_after_tpc():
                 go.Histogram2d(x=(self.res_measure.cl_pds[f"{pl}x"].cl_pos_x - self.res_measure.cl_pds[f"{pl}x"].pos_tpc) * self.pitch,
                                y=self.res_measure.cl_pds[f"{pl}x"].res_x,
                                ybins={"start": y_range[0], "end": y_range[1], "size": (y_range[1] - y_range[0]) / 100},
-                               xbins={"start": x_range[0], "end": x_range[1], "size": (x_range[1] - x_range[0]) / x_range[1]},
+                               xbins={"start": x_range[0], "end": x_range[1], "size": (x_range[1] - x_range[0]) / 100},
                                colorscale="viridis",
                                showlegend=False,
                                showscale=False, ),
@@ -966,7 +966,7 @@ class plotter_after_tpc():
 
         for pl in range(0, 4):
             fig.add_trace(
-                go.Box(x=(self.res_measure.cl_pds[f"{pl}x"].cl_pos_x - self.res_measure.cl_pds[f"{pl}x"].pos_tpc) * self.pitch,
+                go.Box(x=((self.res_measure.cl_pds[f"{pl}x"].cl_pos_x - self.res_measure.cl_pds[f"{pl}x"].pos_tpc) * self.pitch//0.10)*0.10,
                        y=self.res_measure.cl_pds[f"{pl}x"].res_x,
                        name=f"Box det {pl}"),
                 col=pl // 2 + 1, row=pl % 2 + 1 + 2)
