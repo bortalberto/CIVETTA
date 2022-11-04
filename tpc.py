9689712273f55192c3bf0d9e4aa8b29539236a2b
borta <bortalberto@gmail.com>
2022-11-04 08:45:07 +0100

    Feather format addeding to monitor

--- tpc.py
+++ tpc.py
@@ -786,7 +786,8 @@ class plotter_after_tpc():
     def plot_residuals_vs_pos(self):
         fig = make_subplots(rows=4, cols=2,
                             # row_heights=[800,800,800,800],
-                            subplot_titles=("Detector 0", "Detector 1", "Detector 2", "Detector 3","Detector 0", "Detector 1", "Detector 2", "Detector 3"))
+                            subplot_titles=("Detector 0", "Detector 1", "Detector 2", "Detector 3","Detector 0", "Detector 1", "Detector 2", "Detector 3"),
+                            specs = ("","","","",{"secondary_y": True},{"secondary_y": True},{"secondary_y": True},{"secondary_y": True}))
         for pl in range(0, 4):
             fig.add_trace(
                 go.Histogram2d(x=self.res_measure.cl_pds[f"{pl}x"].cl_pos_x * 0.0650,
