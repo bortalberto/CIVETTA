d618238f655e96117478dbec30d1e660d89a8ec1
bortalberto <bortalberto@gmail.com>
2022-11-10 10:57:28 +0100

    Implementing tpc post analysis plot

--- run_civetta.py
+++ run_civetta.py
@@ -11,4 +11,5 @@ os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down 20")
 os.system(f"./civetta.py -ca_al {run} -cpu 35")
 os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down {down_sampling}")
 os.system(f"./civetta_perf.py {run} -perf -sT 20 -sD 6 -chi -cpu 20;")
-#./civetta_perf.py {run} -Ser;
+os.system(f"./civetta_perf.py {run} -Ser;")
+
