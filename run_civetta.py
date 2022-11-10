e9c14b01030125116394b7339b5fbbba105a4fbe
bortalberto <bortalberto@gmail.com>
2022-11-10 11:43:02 +0100

    Implementing tpc post analysis plot

--- run_civetta.py
+++ run_civetta.py
@@ -7,9 +7,11 @@ if len(sys.argv)>2:
 else:
     down_sampling = 2
 
-os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down 20")
-os.system(f"./civetta.py -ca_al {run} -cpu 35")
-os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down {down_sampling}")
-os.system(f"./civetta_perf.py {run} -perf -sT 20 -sD 6 -chi -cpu 20;")
-os.system(f"./civetta_perf.py {run} -Ser;")
+# os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down 20")
+# os.system(f"./civetta.py -ca_al {run} -cpu 35")
+# os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down {down_sampling}")
+# os.system(f"./civetta_perf.py {run} -perf -sT 20 -sD 6 -chi -cpu 20;")
+# os.system(f"./civetta_perf.py {run} -Ser;")
+os.system(f"./civetta_tpc.py {run} -thr;")
+os.system(f"./civetta_tpc.py {run} -tw;")
 
