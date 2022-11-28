1bc9ad385957b9a395a1a633a4a7d72373f6f704
bortalberto <bortalberto@gmail.com>
2022-11-28 09:43:57 +0100

    Adding clean option to tpc runner

--- run_civetta.py
+++ run_civetta.py
@@ -7,8 +7,8 @@ if len(sys.argv)>2:
 else:
     down_sampling = 2
 angle=0
-os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down 20")
-os.system(f"./civetta.py -ca_al {run} -cpu 35")
 os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down {down_sampling}")
+os.system(f"./civetta.py -ca_al {run} -cpu 35")
+# os.system(f"./civetta.py -d -a -c -c2 -cpu 30 {run} -down {down_sampling}")
 os.system(f"./civetta_perf.py {run} -perf -sT 20 -sD 6 -chi -cpu 20;")
 os.system(f"./civetta_perf.py {run} -Ser;")
